2b083769e9094a8f6761cfc654bec5186ec2b025
WYLLIAM MANKOWSKI FLORES <ALU201523207@esade.local>
2019-06-06 20:34:01 -0300

    ClassMainActivity

--- app/src/main/java/com/example/projetolistas/MainActivity.java
+++ app/src/main/java/com/example/projetolistas/MainActivity.java
@@ -1,52 +1,68 @@
 package com.example.projetolistas;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+
+import com.example.newprojetolistadecompras.JavaClasses.Produto;
+
+import java.util.List;
+
+public class MainActivity extends Activity {
+
+    private FloatingActionButton addLista;
+
+    ListView MinhasListas;
+    List<Produto> Lista;
+    AdapterProduto adapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        addLista = (FloatingActionButton) findViewById(R.id.addLista);
+
+        MinhasListas = (ListView)findViewById(R.id.minhasListas);
+
+        addLista.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+
+                startActivity(new Intent(MainActivity.this, AddprodutoActivity.class));
+
             }
         });
+
+
+    }
+
+    private void carregarLista(){
+
+        Lista = ProdutoDAO.listar(this);
+
+        Log.i("cont","total: " + Lista.size());
+
+//manter        adapter = new ArrayAdapter(this,
+//manter        android.R.layout.simple_list_item_1, lista );
+        adapter = new AdapterProduto(this, Lista);
+        MinhasListas.setAdapter(adapter);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onResume() {
+        super.onResume();
+        carregarLista();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
-    }
 }

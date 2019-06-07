895224e6714594063c1a8d64469188ba668a8ac0
Giulia Soares Rego dos Santos <giulias@dbserver.com.br>
2019-06-06 21:25:41 -0300

    Adicionando layout/activity_layout_lista_produtos

---
+++ app/src/main/java/com/example/projetolistas/layout_lista_produtos.java
@@ -0,0 +1,13 @@
+package com.example.projetolistas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class layout_lista_produtos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_lista_produtos);
+    }
+}

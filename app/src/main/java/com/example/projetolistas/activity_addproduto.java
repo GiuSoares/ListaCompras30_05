dcfad90a9f921dcdd2fe40f44b0fdcafa77c72a3
WYLLIAM MANKOWSKI FLORES <ALU201523207@esade.local>
2019-06-06 21:37:30 -0300

    LayoutaddProduto

---
+++ app/src/main/java/com/example/projetolistas/activity_addproduto.java
@@ -0,0 +1,13 @@
+package com.example.projetolistas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_addproduto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addproduto);
+    }
+}
